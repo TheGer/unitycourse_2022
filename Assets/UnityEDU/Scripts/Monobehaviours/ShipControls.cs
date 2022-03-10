943fdebe950a2da1a7b1c0577bc04a9f1d888be9
Gerard Said <gerry@Users-MacBook-Pro-2.local>
2022-03-10 17:57:41 +0100

    my files

---
+++ Assets/UnityEDU/Scripts/Monobehaviours/ShipControls.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShipControls : MonoBehaviour {
+
+
+    //Get the turret rotator so we have the correct
+    //transform directions
+    Transform turretRotator;
+
+    [Header("Don't forget to put a bullet here" +
+        "Never Leave without ammunition")]
+    [SerializeField] GameObject bullet;
+    //use the rigibody to move ship around. 
+    Rigidbody rb;
+
+    //capture the direction the ship is supposed to move. 
+    float xDirection;
+    float yDirection;
+
+    //BulletAnchor to collect bullets in the hierarcy view
+    public GameObject bulletAnchor;
+
+    //expose the move speed for a designer
+    [Header ("Set ship speed here, Min 0 Max 10")]
+
+    [Range(0,10)]
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        turretRotator = transform.Find("TurretRotator");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveShip();
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                FireBullet();
+            }
+
+	}
+
+    //this method controls ship movement and looks for axis direction
+
+    public void MoveShip()
+    {
+        xDirection = CrossPlatformInputManager.GetAxis("Horizontal");
+        yDirection = CrossPlatformInputManager.GetAxis("Vertical");
+        rb.velocity = new Vector3(xDirection * moveSpeed, yDirection * moveSpeed, 0);
+
+    }
+
+    //Fire a bullet on left mouse click
+    //lock it to anchor point "BULLETANCHOR"
+    //so we can store the bullets somewhere easy
+
+    public void FireBullet()
+    {
+        GameObject firedBullet = Instantiate<GameObject>(bullet);
+        firedBullet.transform.SetParent(bulletAnchor.transform);
+        firedBullet.transform.position = turretRotator.position;
+        firedBullet.transform.rotation = turretRotator.rotation;
+
+    }
+}
---
+++ Assets/UnityEDU/Scripts/Monobehaviours/bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class bullet : MonoBehaviour {
+
+    //how long will the bullet stay on screen?
+    public float aliveTime;
+
+    //Set the bullet Speed leave available for designer
+    public float bulletSpeed;
+
+    //Get the bullets rigidbody to set and control velocity
+    Rigidbody bulletRb;
+
+    //a bool to store whether or not this has collided
+
+    public bool collided = false;
+
+    public bool Collided { get { return collided; } }
+
+   
+
+
+    Vector3 fireDirection;
+	// Use this for initialization
+	void Start () {
+
+        bulletRb = gameObject.GetComponent<Rigidbody>();
+        bulletRb.velocity = transform.forward * bulletSpeed;
+        DestroyBullet(aliveTime);
+    }
+	
+
+
+    public void DestroyBullet(float t)
+    {
+        Destroy(gameObject, t);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //collided = true;
+        Destroy(gameObject);
+    }
+}
