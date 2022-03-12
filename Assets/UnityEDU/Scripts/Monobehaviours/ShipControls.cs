a5b628990d52ffab977736c10ae081b453a7fab2
deg <david.deguara@mcast.edu.mt>
2022-03-12 20:55:44 +0100

    Implemented bullet pool
    Pending bugs:
    - Bullet OnBecameInvisible() not working as intended
    - Vector3Extensions() ship going down does not appear up @Gerard.

--- Assets/UnityEDU/Scripts/Monobehaviours/ShipControls.cs
+++ Assets/UnityEDU/Scripts/Monobehaviours/ShipControls.cs
@@ -4,96 +4,87 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent(typeof(Rigidbody))]
-public class ShipControls : MonoBehaviour {
-
-
+public class ShipControls : MonoBehaviour
+{
     //Get the turret rotator so we have the correct
     //transform directions
-    Transform turretRotator;
+    Transform _turretRotator;
 
-    [Header("Don't forget to put a bullet here" +
-        "Never Leave without ammunition")]
-    [SerializeField] GameObject bullet;
+    [Header("Don't forget to put a bullet here. Never Leave without ammunition!")]
+    [SerializeField]
+    GameObject bullet;
+
+    [Header("How fast is your bullet?")]
+    [SerializeField]
+    private float fireRate = 25f;
+    
+    
     //use the rigibody to move ship around. 
-    Rigidbody rb;
+    Rigidbody _rb;
 
     //capture the direction the ship is supposed to move. 
-    float xDirection;
-    float yDirection;
-
-    //BulletAnchor to collect bullets in the hierarcy view
-    public GameObject bulletAnchor;
+    private float _xDirection, _yDirection;
+    
 
     //expose the move speed for a designer
-    [Header ("Set ship speed here, Min 0 Max 10")]
+    [Header("Set ship speed here, Min 0 Max 10")] [Range(0, 10)] [SerializeField]
+    private float moveSpeed;
 
-    [Range(0,10)]
-    public float moveSpeed;
-
-	// Use this for initialization
-	void Start () {
-        rb = gameObject.GetComponent<Rigidbody>();
-        turretRotator = transform.Find("TurretRotator");
-        
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody>();
+        _turretRotator = transform.Find("TurretRotator");
+    }
 
 
     void TurretLookAt()
     {
-
         Vector3 mousePos = new Vector3(Input.mousePosition.x,
             Input.mousePosition.y, 0f);
         
-        // --- Fixed turret rotation by replacing LookAt with Pythagoras
-        //turretRotator.transform.LookAt(Camera.main.ScreenToWorldPoint(mousePos));
-
         //Calculating angle using Pythagoras
         Vector2 delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
         //Setting rotation
         transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
-
     }
 
     // Update is called once per frame
-	void Update () {
+    void Update()
+    {
         MoveShip();
         TurretLookAt();
-
-            if(Input.GetMouseButtonDown(0))
-            {
-                FireBullet();
-            }
-
-          
-            
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
     }
-    
-    
+
 
     //this method controls ship movement and looks for axis direction
 
-    public void MoveShip()
+    private void MoveShip()
     {
-        
-        xDirection = CrossPlatformInputManager.GetAxis("Horizontal");
-        yDirection = CrossPlatformInputManager.GetAxis("Vertical");
-        rb.velocity = new Vector3(xDirection * moveSpeed, yDirection * moveSpeed, 0);
+        _xDirection = CrossPlatformInputManager.GetAxis("Horizontal");
+        _yDirection = CrossPlatformInputManager.GetAxis("Vertical");
+        _rb.velocity = new Vector3(_xDirection * moveSpeed, _yDirection * moveSpeed, 0);
         transform.position = transform.position.keepOnScreen(Camera.main);
-        
     }
 
-    //Fire a bullet on left mouse click
-    //lock it to anchor point "BULLETANCHOR"
-    //so we can store the bullets somewhere easy
-
-    public void FireBullet()
+    private void FireBullet()
     {
-        GameObject firedBullet = Instantiate<GameObject>(bullet);
-        firedBullet.transform.SetParent(bulletAnchor.transform);
-        firedBullet.transform.position = turretRotator.position;
-        firedBullet.transform.rotation = turretRotator.rotation;
+        GameObject b = BulletPool.SharedInstance.GetAvailableBullet();
 
+        if (b != null)
+        {
+            //Spawn bullet in front turret
+            b.transform.position = _turretRotator.position + (transform.up * 1.1f);
+            //Set the bullet at the correct angle
+            Vector3 delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            delta.Normalize();
+            b.GetComponent<Rigidbody>().velocity = delta * fireRate;
+            b.SetActive(true);
+        }
     }
 }
