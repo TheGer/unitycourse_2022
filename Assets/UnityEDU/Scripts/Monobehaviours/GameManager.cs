750af814e2911b5c23a1b299986f64ac0350d698
gerry <gerrysaid@gmail.com>
2022-03-17 19:49:52 +0100

    well done Daren

--- Assets/UnityEDU/Scripts/Monobehaviours/GameManager.cs
+++ Assets/UnityEDU/Scripts/Monobehaviours/GameManager.cs
@@ -1,6 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+class LevelInfo
+{
+	
+	public string levelProgression = "1:3/2,2:4/2,3:3/3,4:4/3,5:5/3,6:3/4,7:4/4,8:5/4,9:6/4,10:3/5";
+
+
+	private int currentLevel, numberOfAsteroids, numberOfChildren;
+
+	void ParseLevelProgression()
+	{
+		foreach (string l in levelProgression.Split(','))
+		{
+			string[] eachLevel = l.Split(':');
+			currentLevel = Int32.Parse(eachLevel[0]);
+			numberOfAsteroids = Int32.Parse(eachLevel[0].Split('/')[0]);
+			numberOfChildren = Int32.Parse(eachLevel[0].Split('/')[1]);
+
+		}
+	}
+
+
+}
 
 public class GameManager : Singleton<GameManager> {
 
@@ -9,6 +34,8 @@ public class GameManager : Singleton<GameManager> {
 	// Use this for initialization
 	public asteroidData[] asteroids;
 	public int numberOfClusters;
+	LevelInfo currentLevel;
+	
 	
 	
 	void Start () {
